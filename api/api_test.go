579d8a070b7974ac86b37a2356188dd7fc222bc1
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-04-19 16:25:51 -0400

    fix test

--- api/api_test.go
+++ api/api_test.go
@@ -16,7 +16,7 @@ import (
 func TestSimpleSequence(t *testing.T) {
 	topic := "testing"
 	offsetsTopic := "testing_offsets"
-	partitionsTopic := "testing"
+	partitionsTopic := "testing_p"
 	pm1 := partitions.Map{}
 	pm2 := partitions.Map{}
 	lc1, err := log_consumer.NewLogConsumer("lc1", "cg1", topic, offsetsTopic, partitionsTopic, "localhost:11000", []string{"localhost:19092", "localhost:29092", "localhost:39092"}, 60000, &pm1)
